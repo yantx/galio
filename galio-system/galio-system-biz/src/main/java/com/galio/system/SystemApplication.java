c5fcf3e0c8f91932b87c5cc893fe6120d3f6f696
yantx <yantxd@qq.com>
2023-05-25 08:20:28 +0800

    服务多环境配置文件日志配置提交

--- galio-system/galio-system-biz/src/main/java/com/galio/system/SystemApplication.java
+++ galio-system/galio-system-biz/src/main/java/com/galio/system/SystemApplication.java
@@ -1,12 +1,11 @@
 package com.galio.system;
 
 import com.galio.core.constant.CommonConstants;
-import com.galio.mybatis.config.MybatisPlusConfiguration;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
-import org.springframework.context.annotation.Import;
 
 /**
  * @Author: galio
