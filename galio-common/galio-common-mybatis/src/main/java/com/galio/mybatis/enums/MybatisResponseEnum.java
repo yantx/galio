3f89cdf5772a97b30bf12eb88fad8122b2113d36
yantx <yantxd@qq.com>
2023-07-18 08:09:40 +0800

    增加自定义消息体

---
+++ galio-common/galio-common-mybatis/src/main/java/com/galio/mybatis/enums/MybatisResponseEnum.java
@@ -0,0 +1,30 @@
+package com.galio.mybatis.enums;
+
+import com.galio.core.enums.StatusCode;
+import lombok.Getter;
+
+/**
+ * @Author: galio
+ * @Date: 2023-07-17 07:28:18
+ * @Description: mybatis自定义响应码
+ */
+@Getter
+public enum MybatisResponseEnum implements StatusCode {
+
+    SET_FIELD_VALUE_ERROR(90001,"set.field.value.error");
+
+    private int code;
+    private String msg;
+
+    private Object[] args;
+
+    MybatisResponseEnum(int code, String msg){
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public MybatisResponseEnum packageByArgs(Object... args) {
+        this.args = args;
+        return this;
+    }
+}
