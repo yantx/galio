53827fe2bdee36e09a15ed68ee3180f8a76152fd
yantx <yantxd@qq.com>
2023-09-05 22:06:03 +0800

    清理无用文件

--- galio-ui/src/config/website.config.ts
+++
@@ -1,9 +0,0 @@
-import logoImage from '@/assets/images/logo.png';
-import loginImage from '@/assets/images/account-logo.png';
-
-export const websiteConfig = Object.freeze({
-  title: 'galio',
-  logo: logoImage,
-  loginImage: loginImage,
-  loginDesc: 'galio平台',
-});
