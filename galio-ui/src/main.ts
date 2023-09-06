323216334d51e149810276fae094620f65385a10
yantx <yantxd@qq.com>
2023-09-06 21:19:32 +0800

    前端工程集成pinia

--- galio-ui/src/main.ts
+++ galio-ui/src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import store from './store'
 
-createApp(App).mount('#app')
+//创建Vue实例
+const app = createApp(App)
+
+// 挂载pinia
+app.use(store)
+
+// 挂载实例
+app.mount('#app');
