bc143257dc7ebbd0dabbfd62489f3a85e400fabb
yantx <yantxd@qq.com>
2024-03-16 06:54:24 +0800

    前端request-util代码更新

--- galio-ui/src/utils/request/index.js
+++ galio-ui/src/utils/request/index.js
@@ -2,10 +2,10 @@ import { createAxios } from './invoke'
 
 // 根据base地址判断是否走mock
 let defaultConfig = {
-  baseURL: import.meta.env.VITE_APP_BASE_API,
+  baseURL: import.meta.env.VITE_USE_MOCK ? '/mock' : import.meta.env.VITE_APP_BASE_API,
   timeout: import.meta.env.VITE_APP_API_TIMEOUT || 12000,
 }
 
-export const request = createAxios({ baseURL: '/mock', timeout: defaultConfig.timeout })
+export const request = createAxios(defaultConfig)
 
 export const mockRequest = createAxios({ baseURL: '/mock', timeout: defaultConfig.timeout })
